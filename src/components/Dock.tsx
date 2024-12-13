2dcfa3069fc1c72ae56fa08539eb7002da745647
Bastien GRISVARD <bastien.grisvard@raccourci.fr>
2024-12-13 19:20:40 +0100

    Fix type error in Dock.tsx

--- src/components/Dock.tsx
+++ src/components/Dock.tsx
@@ -12,14 +12,14 @@ export const Dock = () => {
   return <div className="h-full" style={{
     backgroundColor: 'rgba(255,255,255,0.7)'
   }}>
-    <div className="flex flex-col items-center p-2 gap-2" >
+    <div className="flex flex-col items-center p-2 gap-2">
       {
         Object.keys(windows).map((key) => {
           const window = windows[key];
           return <button
             key={key}
             onClick={() => openWindow(key)}
-            className={`p-2 rounded-lg ${activeWindow?.title === window.title ? "bg-gray-600" : "bg-gray-400"}`}
+            className={`p-2 rounded-lg ${activeWindow.find((w) => w.title === window.title) ? "bg-gray-600" : "bg-gray-400"}`}
           >
             {
               <window.icon/>
